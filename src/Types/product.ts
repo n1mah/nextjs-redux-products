6f03c2e9b3f16c11eed725114f5f1fe867fbfe0b
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-19 23:13:08 +0330

    Feat: Add Types(Interfaces)

---
+++ src/Types/product.ts
@@ -0,0 +1,9 @@
+export interface ProductType {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    image: string;
+    sizes: string[];
+    colors: string[];
+}
---
+++ src/Types/review.ts
@@ -0,0 +1,8 @@
+export type ReviewType = {
+    id: string
+    productId: string
+    user: string
+    rating: number
+    comment: string
+    createdAt: string
+}
